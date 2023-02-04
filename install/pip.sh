308bae15779026d124bea6cce5ad7839ead4737d
Wual1014 <105047274+14wual@users.noreply.github.com>
2023-02-04 13:57:03 +0100

    Create pip.sh

---
+++ install/pip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pip install configparser
+pip install cmd
+pip install datetime
+pip install argparse
+pip install sys
+pip install os
+pip install getpass
+pip install subprocess
+pip install webbrowser
+pip install csv
+pip install pyperclip
+pip install urllib
+pip install sqlite3
