f579438b19bdd61df07c1d9d95300efff57564a0
Wual1014 <105047274+14wual@users.noreply.github.com>
2023-02-04 11:56:34 +0100

    Create password.py

---
+++ me/_functs/password.py
@@ -0,0 +1,133 @@
+# ██╗    ██╗██╗   ██╗ █████╗ ██╗     
+# ██║    ██║██║   ██║██╔══██╗██║     
+# ██║ █╗ ██║██║   ██║███████║██║     (code by wual)
+# ██║███╗██║██║   ██║██╔══██║██║     
+# ╚███╔███╔╝╚██████╔╝██║  ██║███████╗
+#  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝
+
+# See proyect >> https://github.com/14wual/me
+# Follow me >> https://twitter.com/codewual
+
+# --------------------Extern Imports--------------------
+try:
+    import pyperclip as clipboard
+    from urllib.parse import urlparse
+    import sqlite3
+    import os
+    import csv
+except ImportError: raise ImportError("Failed to import modules. Make sure it is installed correctly and is in the PYTHONPATH.")
+
+# --------------------APP--------------------
+class Passwd:
+
+    def matter():
+        
+        conn = sqlite3.connect('/home/%s/.config/me/local/share/database/password.db' % os.environ['USER'])
+        cursor = conn.cursor()
+        
+        path = input("\nIt is recommended that the path be found in input\\*.csv. And that this is a relative path within the main folder.\n\n[input\*.csv] Write the path:")
+        if path == "":path = r'*.csv'
+
+        if os.path.exists(path):
+            with open(path, 'r') as file:
+
+                reader = csv.reader(file)
+                next(reader)
+
+                for row in reader:
+                    url, username, password, totp, extra, name, grouping, fav = row
+                    try:
+                        cursor.execute('''
+                        INSERT OR IGNORE INTO passwords (url, username, password, totp, extra, name, grouping, fav)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                        ''', (url, username, password, totp, extra, name, grouping, fav))
+                        print("[ ✓ ] Inserted record.")
+                    except sqlite3.IntegrityError:print("[ ✕ ] Record not inserted.")
+
+        conn.commit()
+        conn.close()
+        
+    def modify():
+        
+        conn = sqlite3.connect('/home/%s/.config/me/local/share/database/password.db' % os.environ['USER'])
+        cursor = conn.cursor()
+
+        search_value = input('Enter a search value: ')
+        
+        cursor.execute(
+            'SELECT url, username, password, name FROM passwords WHERE url LIKE ? OR username LIKE ?',
+            ('%' + search_value + '%', '%' + search_value + '%')
+        )
+
+        results = cursor.fetchall()
+        for i, row in enumerate(results):
+        
+            print(f"""\n
+{i} - URL: {Passwd.short_url(row[0])}
+- Name: {row[3]}
+- Username: {row[1]}""")
+
+        result_number = int(input('[0] Enter the number of a result to display: '))
+
+        selected_row = results[result_number - 1]
+        
+        cursor.execute(
+            'DELETE FROM passwords WHERE url = ? AND username = ? AND name = ?',
+            (selected_row[0], selected_row[1], selected_row[3])
+        )
+        
+        conn.commit()
+        cursor.close()
+        conn.close()
+        
+    def add():
+        
+        conn = sqlite3.connect('/home/%s/.config/me/local/share/database/password.db' % os.environ['USER'])
+        cursor = conn.cursor()
+
+        url = input('Enter the URL: ')
+        username = input('Enter the username: ')
+        password = input('Enter the password: ')
+        name = input('Enter the name: ')
+
+        cursor.execute(
+            'INSERT INTO passwords (url, username, password, name) VALUES (?, ?, ?, ?)',
+            (url, username, password, name)
+        )
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+    
+    def copy():
+        
+        conn = sqlite3.connect('/home/%s/.config/me/local/share/database/password.db' % os.environ['USER'])
+        cursor = conn.cursor()
+
+        search_value = input('Enter a search value: ')
+        
+        cursor.execute(
+            'SELECT url, username, password, name FROM passwords WHERE url LIKE ? OR username LIKE ?',
+            ('%' + search_value + '%', '%' + search_value + '%')
+        )
+
+        results = cursor.fetchall()
+        for i, row in enumerate(results):
+        
+            print(f"""\n
+{i} - URL: {Passwd.short_url(row[0])}
+- Name: {row[3]}
+- Username: {row[1]}""")
+
+        result_number = int(input('[0] Enter the number of a result to display: '))
+
+        selected_row = results[result_number - 1]
+        print(f"""URL: {Passwd.short_url(row[0])} >> Copied to clipboard""")
+        clipboard.copy(selected_row[2])
+
+        conn.close()
+
+    def short_url(url):
+        parsed_url = urlparse(url)
+        domain = parsed_url.netloc    
+        return domain
