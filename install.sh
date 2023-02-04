81061b59884f89a6da0e50dc46941e9583d8dac1
Wual1014 <105047274+14wual@users.noreply.github.com>
2023-02-04 17:57:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,44 @@
 #!/bin/bash
 
-sudo bash install/install_pip.sh
-sudo bash install/pip.sh
+sudo apt install python3-pip
+
+echo "Downloading get-pip.py..."
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
+echo "Installing pip..."
+python get-pip.py
+
+echo "Removing get-pip.py..."
+rm get-pip.py
+echo "Verifying pip installation..."
+pip --version
+
+echo "Installing configparser..."
+pip install configparser
+echo "Installing cmd..."
+pip install cmd
+echo "Installing datetime..."
+pip install datetime
+echo "Installing argparse..."
+pip install argparse
+echo "Installing sys..."
+pip install sys
+echo "Installing os..."
+pip install os
+echo "Installing getpass..."
+pip install getpass
+echo "Installing subprocess..."
+pip install subprocess
+echo "Installing webbrowser..."
+pip install webbrowser
+echo "Installing csv..."
+pip install csv
+echo "Installing pyperclip..."
+pip install pyperclip
+echo "Installing urllib..."
+pip install urllib
+echo "Installing sqlite3..."
+pip install sqlite3
 
 user=$(whoami)
 
@@ -33,7 +70,7 @@ sudo python3 install/about.py
 sudo python3 install/csv.py
 sudo python3 install/database.py
 
-echo "alias me='python3 /home/$user/.config/me/pip/app/me/__init__.py'" >> ~/.config/.zshrc 
-echo "alias me='python3 /home/$user/.config/me/pip/app/me/__init__.py'" >> ~/.config/.bashrc 
+sudo echo "alias me='python3 /home/$user/.config/me/pip/app/me/__init__.py'" >> ~/.config/.zshrc 
+sudo echo "alias me='python3 /home/$user/.config/me/pip/app/me/__init__.py'" >> ~/.config/.bashrc 
 
 notify-send "wual/ME Installed"
