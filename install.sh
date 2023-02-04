38cf0c6e9bdf66d788a268cc5164ee1855bb4c2f
Wual1014 <105047274+14wual@users.noreply.github.com>
2023-02-04 13:25:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,32 @@ user=$(whoami)
 dir_path="/home/$user/.config/me"
 sudo mkdir -p "$dir_path"
 
+sudo mkdir /home/$user/.config/me/bin
+sudo mkdir /home/$user/.config/me/bin/me
 
-sudo mkdir -p /home/$user/.config/me/bin
-sudo mkdir -p /home/$user/.config/me/bin/me
-sudo python3 scripts/install/setup_about.py
+sudo mkdir /home/$user/.config/me/local
+sudo mkdir /home/$user/.config/me/local/share
+sudo mkdir /home/$user/.config/me/local/share/csv
+sudo mkdir /home/$user/.config/me/local/share/database
 
-sudo mkdir -p /home/$user/.config/me/local
-sudo mkdir -p /home/$user/.config/me/local/share
-sudo mkdir -p /home/$user/.config/me/local/share/database
-sudo python3 scripts/install/setup_databases.py
+sudo mkdir /home/$user/.config/me/usr
+sudo mkdir /home/$user/.config/me/usr/browser
+sudo mkdir /home/$user/.config/me/usr/browser/log
+sudo mkdir /home/$user/.config/me/usr/txt
+sudo mkdir /home/$user/.config/me/usr/txt/archive
+sudo mkdir /home/$user/.config/me/usr/txt/log
+
+sudo mkdir /home/$user/.config/me/pip
+sudo mkdir /home/$user/.config/me/pip/app
+sudo mkdir /home/$user/.config/me/pip/app/me
+
+sudo cp me/ /home/$user/.config/me/pip/app/me -r
+
+sudo python3 install/about.py
+sudo python3 install/csv.py
+sudo python3 install/database.py
+
+echo "alias me='python3 /home/$user/.config/me/pip/app/me/__init__.py'" >> ~/.config/.zshrc 
+echo "alias me='python3 /home/$user/.config/me/pip/app/me/__init__.py'" >> ~/.config/.bashrc 
+
+notify-send "wual/ME Installed"
