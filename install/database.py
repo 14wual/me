93553012d252a50e7978a1d6a69e00ca7ee2c8e9
Wual1014 <105047274+14wual@users.noreply.github.com>
2023-02-04 13:24:54 +0100

    Create database.py

---
+++ install/database.py
@@ -0,0 +1,45 @@
+# ██╗    ██╗██╗   ██╗ █████╗ ██╗     
+# ██║    ██║██║   ██║██╔══██╗██║     
+# ██║ █╗ ██║██║   ██║███████║██║     (code by wual)
+# ██║███╗██║██║   ██║██╔══██║██║     
+# ╚███╔███╔╝╚██████╔╝██║  ██║███████╗
+#  ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝
+
+# See proyect >> https://github.com/14wual/me
+# Follow me >> https://twitter.com/codewual
+
+import sqlite3
+import os
+
+path = '/home/%s/.config/me/local/share/database/password.db' % os.environ['USER']
+
+print("[] Connecting to the ddbb...")
+conn = sqlite3.connect(path)
+
+try:cursor = conn.cursor()
+finally:print("[ ✓ ] Established connection.")
+
+print("[] Creating table, setting columns...")
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS passwords (
+    url TEXT,
+    username TEXT,
+    password TEXT,
+    totp TEXT,
+    extra TEXT,
+    name TEXT,
+    grouping TEXT,
+    fav INTEGER
+)
+''')
+
+try:
+    conn.commit()
+    print("[ ✓ ] Successfully configured.")
+except sqlite3.OperationalError:print("[ ✕ ] Configuration failure.")
+
+print("[] Closing connection...")
+try:
+    conn.close()
+    print("[ ✓ ] Connection closed correctly")
+except:print("[ ✕ ] Could not close the connection")
