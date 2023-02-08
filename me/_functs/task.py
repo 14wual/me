26993521518053386132789a5ab1213c2da583c0
Wual1014 <105047274+14wual@users.noreply.github.com>
2023-02-08 09:17:20 +0100

    Update task.py

--- me/_functs/task.py
+++ me/_functs/task.py
@@ -8,33 +8,82 @@
 # See proyect >> https://github.com/14wual/me
 # Follow me >> https://twitter.com/codewual
 
-#UNDER DEVELOPMENT
-
 # --------------------Extern Imports--------------------
 from datetime import datetime
+import os
 import csv
 
 # --------------------APP--------------------
 class Tasks:
     
-    def today():pass
-    def _list():
-
+    def today():
+        
+        today = datetime.now().strftime('%Y-%m-%d')
         filename = "/usr/local/etc/me/local/share/csv/task.csv"
         with open(filename, 'r') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                if row['status'] == 'in time' or row['status'] == 'True' :
-                    print(f"""
-Title: {row[2]}""")
+                if row['for'] == today:
+                    print(f"Task found for today: {row['title']}")
+                    return True
+        print("No task found for today.")
+        return False
+        
+    def _list():
+        
+        filename = "/usr/local/etc/me/local/share/csv/task.csv"
+        with open(filename, 'r') as file:
+            reader = csv.DictReader(file)
+            tasks = list(reader)
+            for i, task in enumerate(tasks):
+                if task['status'] == 'in time' or task['status'] == 'True':print(f"Task {i+1}: {task['title']}")
 
-    def done():pass
+        num = int(input("Enter the task number to see more details: "))
+        task = tasks[num - 1]
+        print(f"""
+Task: {num}
+Title: {task['title']}
+Description: {task['desc']}
+Finished: {task['for']}
+Status: {task['status']}
+              """)
+
+    def done():
+        
+        filename = "/usr/local/etc/me/local/share/csv/task.csv"
+        temp_file = "/usr/local/etc/me/local/share/csv/temp.csv"
+        
+        with open(filename, 'r') as file:
+            reader = csv.DictReader(file)
+            tasks = list(reader)
+            for i, task in enumerate(tasks):
+                if task['status'] == 'in time' or task['status'] == 'True':print(f"Task {i+1}: {task['title']}")
+        
+        task_number = int(input("Enter the task number to mark as done: "))
+        task_count = 0
+
+        with open(filename, 'r') as file, open(temp_file, 'w') as temp:
+            reader = csv.DictReader(file)
+            writer = csv.DictWriter(temp, fieldnames=reader.fieldnames)
+            writer.writeheader()
+
+            for row in reader:
+                task_count += 1
+                if task_count == task_number:
+                    row['status'] = 'done'
+                writer.writerow(row)
+
+        os.remove(filename)
+        os.rename(temp_file, filename)
+        
+        print("Marked as done.")
+
     
     def add():
         
         now = datetime.now()
         
-        formatted_time = now.strftime('%Y-%m-%d %H:%M:%S')
+        formatted_time = now.strftime('%Y-%m-%d')
         title = input("Title: ")
         while title == "":title = input("Title: ")
         
@@ -44,11 +93,12 @@ Title: {row[2]}""")
         if confirm1 == "Y" or confirm1 == "y":
             while True:
                 try:
-                    date_string = input("Enter a date and time in the format %Y-%m-%d %H:%M:%S: ")
-                    date_format = "%Y-%m-%d %H:%M:%S"
-                    datetime_object = datetime.datetime.strptime(date_string, date_format)
+                    date_string = input("Enter a date and time in the format %Y-%m-%d ")
+                    date_format = "%Y-%m-%d"
+                    datetime_object = datetime
+                    datetime.strptime(date_string, date_format)
                     break
-                except:date_string = input("Enter a date and time in the format %Y-%m-%d %H:%M:%S: ")
+                except:date_string = input("Enter a date and time in the format %Y-%m-%d2: ")
                 finally:status = 'in time'
         else:
             date_string = 'none'
