f70417be5aa948091acabf0724b4dc58088528d1
Wual1014 <105047274+14wual@users.noreply.github.com>
2023-02-13 20:20:20 +0100

    Create commands.sh

---
+++ help/commands.sh
@@ -0,0 +1,89 @@
+echo """Browser Commands:
+    browser: search and browse bookmarks.
+    browser --add/-a: add a new bookmark.
+    browser --delete/-d: delete a new bookmark.
+    browser --list/-l: list your bookmarks.
+    search: do a google search.
+    
+    *Shortcuts: [search], [browser], [b], [s], [brow]
+
+[mail]: Send an email.
+    
+[contacts] Contacts Commands:
+    contacts --modify/-m: look for a contact.
+    contacts --search/-s: modify an existing contact.
+    contacts --add/-a: add a new contact.
+    contacts --delete/-d: delete an existing contact.
+    
+    *Shortcuts: [contacts], [cont], [c]
+
+[passwd] Password Commands:
+    passwd --add/-a: Add New Key to Vault.
+    passwd --copy/-c: Search & Copy Key from Vault.
+    passwd --matter: Import Keys from csv to Vault.
+    passwd --modify/-m: Search & Modify Key from Vault.
+    passwd --generate/-gen/-g: Generate a new password.
+    
+    *Shortcuts: [password], [passwd], [p]
+
+[task] Tasks Commands:
+    task --add/-a: Add New Task.
+    task --done/-d: Mark a task as done.
+    task --list/-l: List all pending tasks
+    task --today/-t: Read today's tasks.
+
+    *Shortcuts: [task], [t]
+    
+[notes] Notes Commands:
+    notes --new/-n: Create a new note.
+    notes --read/-r: View all notes, select one write/read.
+    notes --delete/-d: Delete a note.
+    
+    *Shortcuts: [notes], [n]
+    
+[pc] PC Commands:
+    pc --all/-a: Print all logs
+    pc --ram/-r: Current status of: do
+    pc --cpu/-c: Current status of: cpu
+    pc --disks/-d: Current status of: disks
+    pc --processor/-p: Current status of: processor
+
+[check] Check Commands:
+    check --connect/-c: Check connectivity with a web or ip.
+    check --path/-p: Check if a directory exists.
+    check --file/-f: Ceck if a file exists.
+    second options/optional: -w: write the directory, url, ... (string)
+        check --command -w example
+        
+File manager:
+    SAVE:
+        save --save/-s: save the path where you are. (max 3)
+        save --view/-v: view saved paths.
+    ls: shows all files and folders contained in a specific folder.
+    ls -a: shows all files and folders (including hidden ones) contained in a specific folder.
+    cd: change directory.
+    pwd: view current path.
+    
+    cp: Copy and paste files: allows you to copy files from one folder to another.
+    mv: allows you to move files from one folder to another.
+    rm: Delete Files:  Allows the user to delete unnecessary or unwanted files.
+    rm -r:Delete Directories:  Allows the user to delete unnecessary or unwanted directories.
+    mkdir: Create Folders: Allows the user to create new folders to organize their files.
+    view: View File Details: Displays detailed information about a file, such as its size, creation date, and file type.
+    ren: Rename Files: Allows the user to rename a file to make it easier to find
+
+[python] or [py]: Create a python terminal.
+
+GAMES:
+ - To play a game run the following commands: [rps] [gtn] [mga]
+ - To find out more, run the command [games]
+    
+Other Commands:
+    [help] or [h]: display this panel
+    clear/cls: clear terminal
+    info/i: View app information
+    hello/hi: Initial greeting of the app
+    exit/bye: Exit the program
+    
+Do you need more help? >> https://github.com/14wual/me/tree/main/help
+"""
