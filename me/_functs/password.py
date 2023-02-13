c574af68904d66e5fbf9b0b568e88ebe26555654
Wual1014 <105047274+14wual@users.noreply.github.com>
2023-02-13 20:07:41 +0100

    Update password.py

--- me/_functs/password.py
+++ me/_functs/password.py
@@ -15,6 +15,7 @@ try:
     import sqlite3
     import os
     import csv
+    import random
 except ImportError: raise ImportError("Failed to import modules. Make sure it is installed correctly and is in the PYTHONPATH.")
 
 # --------------------APP--------------------
@@ -131,3 +132,19 @@ class Passwd:
         parsed_url = urlparse(url)
         domain = parsed_url.netloc    
         return domain
+    
+class GeneratePasswd:
+    
+    def new():
+        
+        lists = "abcdefghijklmnñopqrstuvwxyzºª!|·#$%&¬/()=?¿¡.:-_,;*{,}ç^[+]0123456789ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+        length = input("[12] (minimum 8) Length: ")
+        if length == '': length = 12
+        while length <= 8:
+            length = input("[12] (minimum 8) Length: ")
+        password = ""
+        for x in range(length):
+            a = random.choice(str(lists))
+            password += a
+        
+        print(password)
